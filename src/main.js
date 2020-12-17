7a7b7b3539ee3bb15f416ace7807bd234612b8e5
Oğuz Ergül <erguloguz@hotmail.com>
2020-12-17 22:59:12 +0300

    global styles declared

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "./styles/app.css"
+import VueRx from 'vue-rx'
+import VuejsClipper from 'vuejs-clipper'
 
+Vue.use(VueRx)
+
+
+Vue.use(VuejsClipper, {
+    components: {
+        clipperBasic: true,
+        clipperPreview: true,
+        clipperUpload:true,
+
+    }
+})
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
